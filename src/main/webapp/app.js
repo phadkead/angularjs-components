6ba4f79607d01ff0637d9a27798b124b9256cb12
Aditi Phadke <aditie2005@gmail.com>
2017-07-03 18:55:20 +0530

    Changed look and feel of the application using angular material

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -17,5 +17,26 @@
 				{ path: '/Tournaments/...', name: 'Tournaments', component: 'tournaments' },
 				{ path: '/Results/...', name: 'Results', component: 'results' }
 			]
-		});
+		}).config(function($mdThemingProvider) {
+
+	// // Extend the red theme with a different color and make the contrast color black instead of white.
+	// // For example: raised button text will be black instead of white.
+	// var neonRedMap = $mdThemingProvider.extendPalette('red', {
+	// 	'500': '#ff0000',
+	// 	'contrastDefaultColor': 'dark'
+	// });
+
+	// // Register the new color palette map with the name <code>neonRed</code>
+	// $mdThemingProvider.definePalette('neonRed', neonRedMap);
+
+	// // Use that theme for the primary intentions
+	// $mdThemingProvider.theme('default')
+	// 	.primaryPalette('neonRed');
+
+	 $mdThemingProvider.theme('default')
+    .primaryPalette('indigo')
+    .accentPalette('orange')
+	.backgroundPalette('grey') ;
+
+})
 })(window.angular);
